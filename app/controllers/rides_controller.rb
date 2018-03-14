6cb5967ff1a560543eecfef9ef10cb7820e89637
An Nuyen <annuyen@gmail.com>
2018-03-14 03:36:34 +0000

    automatically backed up by learn

---
+++ app/controllers/rides_controller.rb
@@ -0,0 +1,7 @@
+class RidesController < ApplicationController 
+  def new 
+    @ride = Ride.create(user_id: params[:user_id], attraction_id: params[:attraction_id])
+    flash[:alert] = @ride.take_ride 
+    redirect_to usre_path(@ride.user)
+  end 
+end 
